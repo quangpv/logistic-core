3f63ccd3df83f2b389a486b6660ce98d1c59fed6
quangpv <quangpv.uit@gmail.com>
2020-04-04 23:04:10 +0700

    update delivery detail

--- core/src/main/java/com/support/core/event/Subscriber.kt
+++ core/src/main/java/com/support/core/event/Subscriber.kt
@@ -1,12 +1,30 @@
 package com.support.core.event
 
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import com.support.core.AppExecutors
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 
-class Subscriber<T>(private val owner: LifecycleOwner, private val function: (T) -> Unit) {
-    operator fun invoke(value: T) {
-        if (owner.lifecycle.currentState == Lifecycle.State.DESTROYED) return
-        AppExecutors.mainIO.execute { function(value) }
+class Subscriber<T> : MutableLiveData<T>() {
+
+    fun subscribe(owner: LifecycleOwner, function: (T) -> Unit) {
+        super.observe(owner, object : Observer<T> {
+            override fun onChanged(t: T) {
+                function(t)
+                removeObserver(this)
+            }
+        })
     }
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner, object : Observer<T> {
+            override fun onChanged(t: T) {
+                observer.onChanged(t)
+                removeObserver(this)
+            }
+        })
+    }
+}
+
+fun <T> subscriber(function: Subscriber<T>.() -> Unit): Subscriber<T> {
+    return Subscriber<T>().apply(function)
 }
--- core/src/main/java/com/support/core/functional/FullLifecycleObserver.kt
+++ core/src/main/java/com/support/core/functional/FullLifecycleObserver.kt
@@ -1,31 +1,38 @@
 package com.support.core.functional
 
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
-import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
-interface FullLifecycleObserver : LifecycleObserver {
-    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+interface FullLifecycleObserver : LifecycleEventObserver {
+    override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+        when (event) {
+            Lifecycle.Event.ON_CREATE -> onCreate()
+            Lifecycle.Event.ON_DESTROY -> onDestroy()
+            Lifecycle.Event.ON_START -> onStart()
+            Lifecycle.Event.ON_STOP -> onStop()
+            Lifecycle.Event.ON_RESUME -> onResume()
+            Lifecycle.Event.ON_PAUSE -> onPause()
+            else -> {
+            }
+        }
+    }
+
     fun onCreate() {
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroy() {
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStart() {
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onStop() {
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
     }
 
-    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
     }
 }
