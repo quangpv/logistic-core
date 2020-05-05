56eb12bed8bcc93ea62e1a593df54df74d6f5e06
quangpv <quangpv.uit@gmail.com>
2020-05-05 20:49:23 +0700

    update

--- core/src/main/java/com/support/core/base/BaseFragment.kt
+++ core/src/main/java/com/support/core/base/BaseFragment.kt
@@ -2,16 +2,11 @@ package com.support.core.base
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.LifecycleRegistry
-import com.support.core.Dispatcher
-import com.support.core.ResultLifecycle
-import com.support.core.ResultOwner
-import com.support.core.ResultRegistry
+import com.support.core.*
+import com.support.core.extension.lazyNone
 import com.support.core.functional.FragmentVisibleObserver
 import com.support.core.functional.LocalStore
 import com.support.core.functional.LocalStoreOwner
@@ -19,10 +14,13 @@ import com.support.core.functional.LocalStoreOwner
 abstract class BaseFragment(contentLayoutId: Int) : Fragment(contentLayoutId), ResultOwner,
     LocalStoreOwner, Dispatcher {
     private val mResultRegistry = ResultRegistry()
-    private val visibleRegistry get() = visibleOwner.lifecycle as VisibleLifecycleRegistry
+    private val visibleRegistry get() = visibleOwner.lifecycle as ViewLifecycleRegistry
 
-    override val localStore: LocalStore by lazy(LazyThreadSafetyMode.NONE) { LocalStore() }
-    val visibleOwner by lazy(LazyThreadSafetyMode.NONE) { VisibleLifecycleOwner(this) }
+    override val localStore: LocalStore by lazyNone { LocalStore() }
+
+    open val visibleOwner: LifecycleOwner by lazyNone {
+        VisibleLifecycleOwner(this)
+    }
 
     override val resultLife: ResultLifecycle get() = mResultRegistry
 
@@ -63,7 +61,8 @@ abstract class BaseFragment(contentLayoutId: Int) : Fragment(contentLayoutId), R
     }
 
     override fun onHiddenChanged(hidden: Boolean) {
-        visibleRegistry.hide(hidden)
+        val registry = visibleRegistry as? VisibleLifecycleRegistry ?: return
+        registry.hide(hidden)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -80,111 +79,4 @@ abstract class BaseFragment(contentLayoutId: Int) : Fragment(contentLayoutId), R
         mResultRegistry.handlePermissionsResult(requestCode, permissions, grantResults)
     }
 
-}
-
-class VisibleLifecycleRegistry(provider: LifecycleOwner, private val fragment: Fragment) :
-    LifecycleRegistry(provider) {
-    companion object {
-        private const val STATE_NONE = -1
-        private const val STATE_CREATED = 1
-        private const val STATE_DESTROYED = 2
-        private const val STATE_STARTED = 3
-        private const val STATE_STOPPED = 4
-        private const val STATE_RESUMED = 5
-        private const val STATE_PAUSED = 6
-
-        private val EVENT = hashMapOf(
-            STATE_CREATED to Event.ON_CREATE,
-            STATE_DESTROYED to Event.ON_DESTROY,
-            STATE_STARTED to Event.ON_START,
-            STATE_STOPPED to Event.ON_STOP,
-            STATE_RESUMED to Event.ON_RESUME,
-            STATE_PAUSED to Event.ON_PAUSE
-        )
-    }
-
-    private var mCurrentState = STATE_NONE
-
-    private val Fragment.isVisibleOnScreen: Boolean
-        get() = !isHidden && (parentFragment?.isVisibleOnScreen ?: true)
-
-    private val childVisibleLifecycle
-        get() = (fragment.childFragmentManager.fragments
-            .find { !it.isHidden } as? BaseFragment)
-            ?.visibleOwner?.lifecycle as? VisibleLifecycleRegistry
-
-    fun create() {
-        next(STATE_CREATED)
-    }
-
-    fun destroy() {
-        next(STATE_DESTROYED)
-    }
-
-    fun start() {
-        if (fragment.isVisibleOnScreen) next(STATE_STARTED)
-    }
-
-    fun stop() {
-        if (fragment.isVisibleOnScreen) next(STATE_STOPPED)
-    }
-
-    fun resume() {
-        if (fragment.isVisibleOnScreen) next(STATE_RESUMED)
-    }
-
-    fun pause() {
-        if (fragment.isVisibleOnScreen) next(STATE_PAUSED)
-    }
-
-    fun hide(hidden: Boolean) {
-        if (hidden) {
-            hidePause()
-            hideStop()
-        } else {
-            showStart()
-            showResume()
-        }
-    }
-
-    private fun hidePause() {
-        childVisibleLifecycle?.hidePause()
-        next(STATE_PAUSED)
-
-    }
-
-    private fun hideStop() {
-        childVisibleLifecycle?.hideStop()
-        next(STATE_STOPPED)
-    }
-
-    private fun showStart() {
-        next(STATE_STARTED)
-        childVisibleLifecycle?.showStart()
-    }
-
-    private fun showResume() {
-        next(STATE_RESUMED)
-        childVisibleLifecycle?.showResume()
-    }
-
-
-    private fun next(state: Int): VisibleLifecycleRegistry {
-        if (mCurrentState == state) return this
-        val event = EVENT[state] ?: error("Not accept state $state")
-        Log.i("LifecycleEvent", "${fragment.javaClass.simpleName} - $event")
-        handleLifecycleEvent(event)
-        mCurrentState = state
-        return this
-    }
-
-}
-
-class VisibleLifecycleOwner(fragment: Fragment) : LifecycleOwner {
-    private val mLifecycle =
-        VisibleLifecycleRegistry(this, fragment)
-
-    override fun getLifecycle(): Lifecycle {
-        return mLifecycle
-    }
 }
