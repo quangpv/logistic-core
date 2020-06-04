6df3a1c450c53180bb11885550d16f7e89134a33
quangpv <quangpv.uit@gmail.com>
2020-06-04 23:28:34 +0700

    update ui

--- core/src/main/java/com/support/core/extension/BitmapExt.kt
+++ core/src/main/java/com/support/core/extension/BitmapExt.kt
@@ -1,6 +1,7 @@
 package com.support.core.extension
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Base64
 import java.io.ByteArrayOutputStream
 
@@ -12,4 +13,15 @@ fun Bitmap.toBase64(recycle: Boolean): String {
     if (recycle) recycle()
 //    return "data:image/jpeg;base64,${Base64.encodeToString(byteArray, Base64.DEFAULT)}"
     return Base64.encodeToString(byteArray, Base64.DEFAULT)
+}
+
+
+fun Bitmap.toBytes(): ByteArray {
+    val stream = ByteArrayOutputStream()
+    compress(Bitmap.CompressFormat.PNG, 100, stream)
+    return stream.toByteArray()
+}
+
+fun bitmapFrom(byteArray: ByteArray): Bitmap? {
+    return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
 }
