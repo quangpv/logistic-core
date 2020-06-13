248ec6eebef455288342d74d196a535f726047d3
quangpv <quangpv.uit@gmail.com>
2020-06-13 10:28:20 +0700

    update auto focus and compress image

--- core/src/main/java/com/support/core/helpers/FileCache.kt
+++ core/src/main/java/com/support/core/helpers/FileCache.kt
@@ -7,18 +7,21 @@ import java.io.File
 import java.io.FileOutputStream
 
 @Inject(true)
-class FileCache(private val context: Context) {
+class FileCache(private val context: Context, private val folderName: String = "photos") {
     fun delete(signaturePath: String) {
         File(signaturePath).delete()
     }
 
     fun save(it: Bitmap, quality: Int = 80): String {
         val currentTime = System.currentTimeMillis()
-        val path = "${context.cacheDir}/$currentTime.jpg"
-        FileOutputStream(path).use { out ->
+
+        val folder = File("${context.cacheDir}/$folderName")
+        folder.mkdirs()
+        val file = File(folder, "$currentTime.jpg")
+        FileOutputStream(file).use { out ->
             it.compress(Bitmap.CompressFormat.JPEG, quality, out)
         }
-        return path
+        return file.path
     }
 
 }
